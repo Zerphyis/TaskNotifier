8eb900e286a1bc739a2f194263c722950d5a1086
Otavio <estudosprogotavio@gmail.com>
2025-05-06 06:02:41 -0300

    adicionando classe Application

---
+++ src/main/java/dev/Zerpyhis/TaskNotifier/TaskNotifierApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerpyhis.TaskNotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskNotifierApplication.class, args);
+	}
+
+}
